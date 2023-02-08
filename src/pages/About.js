fd8fc32adde5c209c10af6e68c32bdead77f98cd
anitasilvi <anitaferdina06@gmail.com>
2023-02-08 16:11:43 +0700

    Navigate Component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 export default function About() {
+  const [user, setUser] = useState("Anita");
+  if (!user) {
+    return <Navigate to="/" replace={true} />;
+  }
   return (
     <div className="about">
       <h2>About Us</h2>
@@ -17,6 +24,7 @@ export default function About() {
         aliquid quaerat ab, non, facere optio, a quas aspernatur ex explicabo?
         Culpa unde impedit magnam laboriosam omnis numquam earum?
       </p>
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   );
 }
